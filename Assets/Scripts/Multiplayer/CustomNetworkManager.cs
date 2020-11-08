028882bd3be483808da5c1dca3601938c38b4812
Caoilinn Hughes <caoilinnhughes716@gmail.com>
2020-11-08 18:05:12 +0000

    Prefabs for player and network objects

--- Assets/Scripts/Multiplayer/CustomNetworkManager.cs
+++ Assets/Scripts/Multiplayer/CustomNetworkManager.cs
@@ -5,6 +5,19 @@ namespace Multiplayer
 {
     public class CustomNetworkManager : NetworkManager
     {
-    
+        private static CustomNetworkManager instace;
+
+        public static CustomNetworkManager Instace
+        {
+            get
+            {
+                if (instace == null)
+                {
+                    instace = FindObjectOfType<CustomNetworkManager>();
+                }
+                return instace;
+            }
+        }
+
     }
 }
