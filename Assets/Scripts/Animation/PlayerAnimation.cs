2df079127dbfc89f274f76815d2a0344bf250940
unknown <D00198871@student.dkit.ie>
2020-12-02 23:28:57 +0000

    Finished refactor player attack logic code(changed time-based to animation based and remove hundred lines of code)#Item 30443 Debugging Player double script and make sure its working without bugs.#Item 30496 Finished developing the collision interaction between the player's block and enemy's heavy attack. #Item 30491 Debugging the bugs that the player's block action cant respond to all conditions of enemy's heavy attack. #Item 30498

--- Assets/Scripts/Animation/PlayerAnimation.cs
+++ Assets/Scripts/Animation/PlayerAnimation.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    private Animator _anim;
+    public Animator _anim;
     private PlayerAction playerAction;
+    private PlayerControl playerControl;
     private PlayerBehaviour playerBehaviour;
     private PlayerJump playerJump;
     private DoubleJump doubleJump;
     public GameObject enemy;
     private EnemyAction enemyAction;
 
-
     void Start()
     {
         _anim = GetComponent<Animator>();
         playerAction = GetComponent<PlayerAction>();
+        playerControl = GetComponent<PlayerControl>();
         playerBehaviour = GetComponent<PlayerBehaviour>();
         playerJump = GetComponent<PlayerJump>();
         doubleJump = GetComponent<DoubleJump>();
@@ -33,17 +34,23 @@ public class PlayerAnimation : MonoBehaviour
 
     void initialiseAnimatorBool()
     {
+        #region Player Block
         _anim.SetBool("isKeepBlocking", playerAction.isKeepBlocking);
         _anim.SetBool("isPerfectBlock", playerAction.isPerfectBlock);
         _anim.SetBool("isBlockingEnd", playerAction.isBlockingEnd);
+        #endregion
+        #region Jump
         _anim.SetBool("isFirstJump", playerJump.isJump);
         _anim.SetBool("isSecondJump", doubleJump.isDoubleJump);
         _anim.SetBool("isFalling", playerJump.isFalling);
         _anim.SetBool("isGrounded", playerJump.isGrounded);
         _anim.SetBool("FallingToGround", playerJump.fallingToGround);
-        _anim.SetBool("isImpact", playerAction.isImpact);
+        #endregion
+
+        _anim.SetBool("isBlockingImpact", playerAction.isBlockingImpact);
+        _anim.SetBool("isGetHurt", playerAction.isHurt);
         _anim.SetBool("isHitPlayer", enemyAction.isHitPlayer);
-        _anim.SetBool("IsEnemyAttack", enemyAction.IsEnemyAttack);
+        _anim.SetBool("IsPerfectBlockTiming", enemyAction.isPerfectBlockTiming);
     }
 
     public void OnAnimation_isPerfectBlock()
@@ -58,14 +65,36 @@ public class PlayerAnimation : MonoBehaviour
 
     public void OnAnimation_isImpact()
     {
-        playerAction.isImpact = true;
-        enemyAction.IsEnemyAttack = false;
+        playerAction.isBlockingImpact = false;
     }
 
     public void OnAnimation_isImpactEnd()
     {
-        playerAction.isImpact = false;
+        //playerAction.isImpact = false;
+        playerControl.isOnAttackAction = false;
     }
+
+    public void OnAnimation_endLightAttack()
+    {
+        playerControl.isOnAttackAction = false;
+    }
+
+    public void OnAnimation_endHeavyAttack()
+    {
+        playerControl.isOnAttackAction = false;
+    }
+
+    public void OnAnimation_isGetCriticalHit()
+    {
+        playerAction.isHurt = false;
+        playerControl.isOnAttackAction = false;
+    }
+    public void OnAnimation_isGetCriticalHitEnd()
+    {
+        playerControl.isOnAttackAction = false;
+    }
+    
+
 
 
 }
---
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "EnemyWeapon" && player.GetComponent<PlayerAction>().isKeepBlocking == true && player.GetComponent<PlayerAction>().isPerfectBlock == false)
+        {
+            player.GetComponent<PlayerAction>().isBlockingImpact = true;
+            collision.gameObject.GetComponent<Collider>().isTrigger = true;
+            Debug.Log("get hit");
+        }
+    }
+}
