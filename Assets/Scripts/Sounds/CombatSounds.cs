152cdcefe9b63f23f2527de7f84114899e13fc3a
Nicholas Daryl <Nicholas.Daryl@prometric.com>
2021-01-19 17:06:37 +0000

    /#30552 - Create Sound Manager
    Creates a Sound Manager to keep track of all the spawned diagetic sounds
    uses animation events to trigger the creation of new sound objects
    this new sound object will then be passed on to the manager to be
    managed

---
+++ Assets/Scripts/Sounds/CombatSounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatSounds : MonoBehaviour
+{
+    public AudioClip lightAttackSound;
+    public GameObject diageticSoundManagerGameObject;
+    private DiageticSoundManager diageticSoundManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        diageticSoundManager = diageticSoundManagerGameObject.GetComponent<DiageticSoundManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnAnimation_IsLightAttackActive()
+    {
+        diageticSoundManager.Add3DSound(lightAttackSound,gameObject.transform.position); 
+        Debug.Log("Light Attack Sound");
+    }
+}
---
+++ Assets/Scripts/Sounds/DiageticSoundManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiageticSoundManager : MonoBehaviour
+{
+    private LinkedList<GameObject> audioSourceList;
+    private Queue<GameObject> finishedAudioSourceQueue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSourceList = new LinkedList<GameObject>();
+        finishedAudioSourceQueue = new Queue<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if finished playing then remove from list.. is it destroyed in memory?.. maybe garbage collector handles that
+
+        foreach(GameObject source in audioSourceList) {
+            AudioSource audioSource = source.GetComponent<AudioSource>();
+            if (!audioSource.isPlaying) { // check if audio is finished playing
+                finishedAudioSourceQueue.Enqueue(source); // add to queue for deletion, modification mid iteration is not allowed
+            }
+        }
+
+        // remove from list, delete sound objects, use the queue
+
+        while (finishedAudioSourceQueue.Count > 0) {
+            GameObject finished = finishedAudioSourceQueue.Dequeue(); // dequeue to get reference to finished audio object
+            audioSourceList.Remove(finished);  //remove from the list, so won't cause any reference issues
+            Destroy(finished); // destroy game object, deleting from list does not destroy game object, still exists in game world
+        }
+    }
+
+    public void Add3DSound(AudioClip audioClip, Vector3 position) {
+        GameObject newSoundObject = new GameObject(); // create a game object, cant directly create a sound source
+
+        newSoundObject.AddComponent<AudioSource>(); // add a sound source component to object
+
+        audioSourceList.AddLast(newSoundObject); // put into the queue
+
+        AudioSource newSource = newSoundObject.GetComponent<AudioSource>(); // get audio source component from created object
+
+        newSource.transform.position = position; // set position of sound
+
+        newSource.spatialBlend = 1.0f; // set 3D Sound
+
+        newSource.clip = audioClip; // assign audio clip
+
+        newSource.Play(); // immediately start playing sound
+    }
+}
